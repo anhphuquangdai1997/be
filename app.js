f2d61b93c3e0d2543f3f30a41d1690e7cf6d8c63
the-movie-phimmoi <daiphu094@gmail.com>
2024-12-22 03:35:34 +0700

    add git

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+const cors = require("cors");
+
+const errorMiddleware = require("./middleware/error");
+
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "config/config.env" });
+}
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+
+// Enable CORS
+app.use(cors({
+  origin:'http://localhost:5173',
+  methods:['POST','GET','DELETE','PUT'],
+  credentials: true, // Cho phép gửi cookie kèm theo yêu cầu
+}
+));
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
+
+// Route Imports
+const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
+
+
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
+app.use("/api/v1", payment);
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
+
+// Middleware for Errors
+app.use(errorMiddleware);
+
+module.exports = app;
